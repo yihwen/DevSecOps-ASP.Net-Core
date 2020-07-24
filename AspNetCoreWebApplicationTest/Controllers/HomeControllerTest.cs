82a809539a7d2ac5fb6144ab3f2c659581b394b4
yihwen <yihwen@singnet.com.sg>
2020-07-24 16:37:41 +0800

    stable dotnet

--- AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
+++ AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
@@ -6,14 +6,6 @@ namespace AspNetCoreWebApplicationTest.Controllers
 {
     public class HomeControllerTest
     {
-        [Fact]
-        public void NoInputParamGetResponseTest()
-        {
-            HelloController controller = new HelloController();
-            var response = controller.Get().Value as Response;
-            Assert.Equal("Hello World!!!", response.output);
-        }
-		
 		[Fact]
         public void IndexTest()
         {
